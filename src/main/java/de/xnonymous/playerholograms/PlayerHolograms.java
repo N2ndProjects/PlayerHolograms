34b4209caf1dd2b411f92e1934989443111cf3d4
XNonymous <56768074+XNonymous-N2@users.noreply.github.com>
2022-08-11 18:12:25 +0200

    :tada: Initial commit

---
+++ src/main/java/de/xnonymous/playerholograms/PlayerHolograms.java
@@ -0,0 +1,16 @@
+package de.xnonymous.playerholograms;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerHolograms extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
